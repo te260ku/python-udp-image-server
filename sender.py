5273ac3a48221d0ba7c3eba7ca1aa3d1b352a792
te260ku <teturou@aw.wakwak.com>
2021-01-06 13:07:25 +0900

    画像を分割して送信する処理を作成

--- sender.py
+++ sender.py
@@ -2,22 +2,32 @@ import socket
 import numpy as np
 import cv2
 from contextlib import closing
+import time
+import base64
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-to_send_addr = ('127.0.0.1', 9999)
+to_send_addr = ('127.0.0.1', 12345)
 
-# img = np.zeros([500, 500], np.uint)
-img = cv2.imread('image/sample-image.jpeg')
+img = cv2.imread('image/sample-image-2.jpeg')
+# img = np.zeros((100, 100, 3))
+# cv2.imwrite('blank.png',img)
+
 
 with closing(udp):
     jpg_str = cv2.imencode('.jpeg', img)
+    
+    # 一括送信
+    # data = jpg_str[1].tobytes()
+    # udp.sendto(data, to_send_addr)
 
     # 画像を分割する
-    for i in np.array_split(jpg_str[1], 100):
-        # 画像の送信
-        udp.sendto(i.tobytes(), to_send_addr)
+    # 3つに分けて送信する
+    for i in np.array_split(jpg_str[1], 3):
+        data = i.tobytes()
+        udp.sendto(data, to_send_addr)
+        print("send: " + str(data))
+        # time.sleep(2)
 
-    # １つのデータが終了したよを伝えるために判断できる文字列を送信する
-    # -> チェックするなら送信する画像のbytes数のほうがいいと思った
-    udp.sendto(b'__end__', to_send_addr)
+    # udp.sendto(b'__end__', to_send_addr)
+
     udp.close()
